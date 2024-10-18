8d89ad2516597c41ff4511942354c9d691dcf6db
Bhavit <83268213+th33-d0ct0r@users.noreply.github.com>
2024-10-18 04:02:03 +0000

    registeredpage made

--- app/dashboard/registered/page.tsx
+++ app/dashboard/registered/page.tsx
@@ -47,10 +47,6 @@ export default function Dashboard() {
     return router.push("/sign-in");
   }
 
-  if (mongoUser.schoolId) {
-    router.push("/dashboard/registered");
-  }
-
   if (!isLoaded || mongoUserLoading) {
     return (
       <div className="flex flex-col w-[100%] h-[100vh] items-center justify-center">
@@ -60,30 +56,6 @@ export default function Dashboard() {
   }
 
   return (
-    <div className="flex flex-col items-center p-[10vw] min-h-[100vh]">
-      <div className="flex flex-col w-[100%] items-start">
-        <h1 className="text-[#fff] mt-[7vh] mb-0 font-semibold text-2xl">
-          Hello {mongoUser?.name?.split(" ")[0]} 👋!
-        </h1>
-        <p className="text-sm">Register your team for the event 😋</p>
-      </div>
-
-      <img src="/illus.png" className="w-[100%] mt-[5vh]" alt="" />
-
-      <p className="text-xs mt-[5vh]">
-        Welcome to Exogenesis 2024, where tech meets entertainment in an
-        immersive experience across two dynamic venues. Enjoy seamless digital
-        access, interactive activities, exclusive tech innovations, live
-        concerts, gaming booths, AR adventures, and smart payment systems. Dive
-        into the future of creativity and technology!
-      </p>
-
-      <button
-        onClick={() => router.push("/eventReg")}
-        className="bg-[#651DFF] w-[100%] mt-[3vh] text-white p-2 rounded-full text-xl h-[7vh] mb-5"
-      >
-        Register
-      </button>
-    </div>
+    <div className="flex flex-col items-center p-[10vw] min-h-[100vh]"></div>
   );
 }
