32af8edde7d3e53bbc6c4a6c0690b7f246d5328b
CasaN0va-bhavit <bhavit.grover@outlook.com>
2024-10-24 21:18:26 +0530

    daming dams

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -1,22 +1,28 @@
 "use client";
 import { useEffect, useRef } from "react";
 import "@google/model-viewer";
+
 const CameraFeed: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
+
   useEffect(() => {
     const getCameraFeed = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
+      if (typeof navigator !== "undefined") {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+          });
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
+        } catch (err) {
+          console.error("Error maardi:", err);
         }
-      } catch (err) {
-        console.error("Error maardi:", err);
       }
     };
+
     getCameraFeed();
+
     return () => {
       if (videoRef.current && videoRef.current.srcObject) {
         const stream = videoRef.current.srcObject as MediaStream;
@@ -26,21 +32,7 @@ const CameraFeed: React.FC = () => {
     };
   }, []);
 
-  return (
-    <div>
-      <video ref={videoRef} autoPlay className="w-[100vw] h-[100vh]"></video>
-      <model-viewer
-        src="/arrowNew.glb"
-        alt="Arrow"
-        camera-controls
-        shadow-intensity="1"
-        style={{position: 'fixed', bottom: '20vh', left: '20vw', width: '60vw'}}
-        ar
-        ar-modes="webxr scene-viewer quick-look"
-        camera-orbit="90deg 0deg 45deg"
-      />
-    </div>
-  );
+  return <video ref={videoRef} autoPlay />;
 };
 
 export default CameraFeed;
