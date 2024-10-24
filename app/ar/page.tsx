62ca84ff83e26a080f47f72537e5487a74266617
CasaN0va-bhavit <bhavit.grover@outlook.com>
2024-10-24 21:59:29 +0530

    changed video element

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -1,38 +1,34 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+import Webcam from "react-webcam";
 import "@google/model-viewer";
 
 const CameraFeed: React.FC = () => {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const webcamRef = useRef<Webcam | null>(null);
 
-  useEffect(() => {
-    const getCameraFeed = async () => {
-      if (typeof navigator !== "undefined") {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-          });
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        } catch (err) {
-          console.error("Error maardi:", err);
-        }
-      }
-    };
-
-    getCameraFeed();
-
-    return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        const stream = videoRef.current.srcObject as MediaStream;
-        const tracks = stream.getTracks();
-        tracks.forEach((track) => track.stop());
-      }
-    };
-  }, []);
-
-  return <video ref={videoRef} autoPlay />;
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{
+          facingMode: "environment",
+        }}
+        className="w-[100vw] h-[100vh]"
+      />
+      <model-viewer
+        src="/arrowNew.glb"
+        alt="Arrow"
+        camera-controls
+        shadow-intensity="1"
+        style={{ position: 'fixed', bottom: '20vh', left: '20vw', width: '60vw' }}
+        ar
+        ar-modes="webxr scene-viewer quick-look"
+        camera-orbit="90deg 0deg 45deg"
+      />
+    </div>
+  );
 };
 
 export default CameraFeed;
